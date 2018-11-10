40cbc2981d3b7252a012f93a59971fecacea89fc
Theodore Zilist <tzilist@gmail.com>
2018-11-09 16:04:33 -0800

    chore: remove formatting

--- examples/named_path.rs
+++ examples/named_path.rs
@@ -1,28 +1,28 @@
 #![feature(async_await, futures_api)]
 
-use tide::head::{Named, NamedComponent};
+use tide::head::{NamedComponent, Named};
 
 struct Number(i32);
 
 impl NamedComponent for Number {
-  const NAME: &'static str = "num";
+    const NAME: &'static str = "num";
 }
 
 impl std::str::FromStr for Number {
-  type Err = std::num::ParseIntError;
+    type Err = std::num::ParseIntError;
 
-  fn from_str(s: &str) -> Result<Self, Self::Err> {
-    s.parse().map(|num| Number(num))
-  }
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        s.parse().map(|num| Number(num))
+    }
 }
 
 async fn add_two(Named(number): Named<Number>) -> String {
-  let Number(num) = number;
-  format!("{} plus two is {}", num, num + 2)
+    let Number(num) = number;
+    format!("{} plus two is {}", num, num + 2)
 }
 
 fn main() {
-  let mut app = tide::App::new(());
-  app.at("add_two/{num}").get(add_two);
-  app.serve("127.0.0.1:8000");
+    let mut app = tide::App::new(());
+    app.at("add_two/{num}").get(add_two);
+    app.serve("127.0.0.1:8000");
 }
