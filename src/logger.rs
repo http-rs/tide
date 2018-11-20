89ad3148a2fc5556a2c50b7b352a2393f55af7b2
Bhargav Voleti <bhargav.voleti93@gmail.com>
2018-11-19 18:05:22 -0600

    Use data stored in Head
    * Remove unecessary types
    * Remove unecessary request method implementation

--- src/logger.rs
+++ src/logger.rs
@@ -4,20 +4,18 @@ use slog_term;
 
 use futures::future::FutureObj;
 
-use std::sync::RwLock;
-
-use crate::{head::Head, Middleware, Request, Response, RouteMatch};
+use crate::{head::Head, Middleware, Response};
 
 /// Root logger for Tide. Wraps over logger provided by slog.SimpleLogger
 ///
 /// Only used internally for now.
-struct RootLogger {
+pub(crate) struct RootLogger {
     // drain: dyn slog::Drain,
     inner_logger: slog::Logger,
 }
 
 impl RootLogger {
-    fn new() -> RootLogger {
+    pub fn new() -> RootLogger {
         let decorator = slog_term::TermDecorator::new().build();
         let drain = slog_term::CompactFormat::new(decorator).build().fuse();
         let drain = slog_async::Async::new(drain).build().fuse();
@@ -27,47 +25,9 @@ impl RootLogger {
     }
 }
 
-struct SimpleLogData {
-    path: String,
-    method: String,
-}
-
-/// General logger used in application middleware.SimpleLogger
-///
-/// Only internal to crate
-pub(crate) struct SimpleLogger {
-    data: RwLock<SimpleLogData>,
-    logger: RootLogger,
-}
-
-impl SimpleLogger {
-    pub fn new() -> Self {
-        SimpleLogger {
-            data: RwLock::new(SimpleLogData {
-                path: String::new(),
-                method: String::new(),
-            }),
-            logger: RootLogger::new(),
-        }
-    }
-}
-
 /// Stores information during request phase and logs information once the response
 /// is generated.
-impl<Data> Middleware<Data> for SimpleLogger {
-    fn request(
-        &self,
-        data: &mut Data,
-        req: Request,
-        params: &RouteMatch<'_>,
-    ) -> FutureObj<'static, Result<Request, Response>> {
-        let mut data = self.data.write().unwrap();
-        data.path = req.uri().path().to_owned();
-        data.method = req.method().as_str().to_owned();
-
-        FutureObj::new(Box::new(async { Ok(req) }))
-    }
-
+impl<Data> Middleware<Data> for RootLogger {
     fn response(
         &self,
         data: &mut Data,
@@ -75,12 +35,11 @@ impl<Data> Middleware<Data> for SimpleLogger {
         resp: Response,
     ) -> FutureObj<'static, Response> {
         let status = resp.status();
-        let data = self.data.read().unwrap();
         info!(
-            self.logger.inner_logger,
+            self.inner_logger,
             "{} {} {}",
-            data.method.clone(),
-            data.path.clone(),
+            head.method(),
+            head.path(),
             status.as_str()
         );
 
