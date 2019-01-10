e705b58accb8f68325bc6c0500c49a65f8461d70
Wonwoo Choi <chwo9843@gmail.com>
2019-01-11 01:55:10 +0900

    Add configuration debug example

--- examples/configuration.rs
+++ examples/configuration.rs
@@ -1,6 +1,7 @@
 #![feature(async_await, futures_api)]
 
-use tide::{head::Path, ExtractConfiguration};
+use futures::future::FutureObj;
+use tide::{head::Path, middleware::RequestContext, ExtractConfiguration, Response};
 
 /// A type that represents how much value will be added by the `add` handler.
 #[derive(Clone, Debug, Default)]
@@ -16,10 +17,16 @@ async fn add(
     format!("{} plus {} is {}", base, amount, base + amount)
 }
 
+fn debug_store(ctx: RequestContext<()>) -> FutureObj<Response> {
+    println!("{:#?}", ctx.store());
+    ctx.next()
+}
+
 fn main() {
     let mut app = tide::App::new(());
     // `App::config` sets the default configuration of the app (that is, a top-level router).
     app.config(IncreaseBy(1));
+    app.middleware(debug_store);
     app.at("add_one/{}").get(add); // `IncreaseBy` is set to 1
     app.at("add_two/{}").get(add).config(IncreaseBy(2)); // `IncreaseBy` is overridden to 2
 
