ddd47656fcca91b292108bfdfb9ad2bb049d8a0a
Wonwoo Choi <chwo9843@gmail.com>
2019-01-15 19:08:47 +0900

    Add status code modifier for IntoResponse

--- examples/default_handler.rs
+++ examples/default_handler.rs
@@ -1,19 +1,13 @@
 #![feature(async_await)]
 
 use http::status::StatusCode;
-use tide::body;
+use tide::IntoResponse;
 
 fn main() {
     let mut app = tide::App::new(());
     app.at("/").get(async || "Hello, world!");
 
-    app.default_handler(async || {
-        http::Response::builder()
-            .status(StatusCode::NOT_FOUND)
-            .header("Content-Type", "text/plain")
-            .body(body::Body::from("¯\\_(ツ)_/¯".to_string().into_bytes()))
-            .unwrap()
-    });
+    app.default_handler(async || "¯\\_(ツ)_/¯".with_status(StatusCode::NOT_FOUND));
 
     let address = "127.0.0.1:8000".to_owned();
     println!("Server is listening on http://{}", address);
