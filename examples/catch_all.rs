d2f61808a59ca6550f28bd7aa3a3657f12e6256d
DCjanus <DCjanus@dcjanus.com>
2019-04-13 08:57:06 +0800

    fix: catch all in 'catch_all'

--- examples/catch_all.rs
+++ examples/catch_all.rs
@@ -3,12 +3,12 @@
 use tide::Context;
 
 async fn echo_path(cx: Context<()>) -> String {
-    let path: String = cx.param("path").unwrap();
+    let path: String = cx.param("path*").unwrap();
     format!("Your path is: {}", path)
 }
 
 fn main() {
     let mut app = tide::App::new(());
-    app.at("/echo_path/:path").get(echo_path);
+    app.at("/echo_path/:path*").get(echo_path);
     app.serve("127.0.0.1:8000").unwrap();
 }
