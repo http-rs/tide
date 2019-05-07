f41305fad889ea0bcb38556acb6c305a9241e1af
k-nasa <htilcs1115@gmail.com>
2019-05-07 10:14:42 +0900

    refactor: Rename Context::app_data to Context::state

--- src/context.rs
+++ src/context.rs
@@ -11,19 +11,19 @@ use std::{str::FromStr, sync::Arc};
 /// Contexts also provide *extensions*, a type map primarily used for low-level
 /// communication between middleware and endpoints.
 pub struct Context<State> {
-    app_data: Arc<State>,
+    state: Arc<State>,
     request: http_service::Request,
     route_params: Params,
 }
 
 impl<State> Context<State> {
     pub(crate) fn new(
-        app_data: Arc<State>,
+        state: Arc<State>,
         request: http::Request<Body>,
         route_params: Params,
     ) -> Context<State> {
         Context {
-            app_data,
+            state,
             request,
             route_params,
         }
@@ -55,8 +55,8 @@ impl<State> Context<State> {
     }
 
     ///  Access app-global data.
-    pub fn app_data(&self) -> &State {
-        &self.app_data
+    pub fn state(&self) -> &State {
+        &self.state
     }
 
     /// Extract and parse a route parameter by name.
