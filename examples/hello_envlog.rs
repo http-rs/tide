225a43d0029a04700442e40833475d2cf7184546
Prasanna Loganathar <pvl@prasannavl.com>
2019-05-21 16:52:50 +0530

    add logging examples

---
+++ examples/hello_envlog.rs
@@ -0,0 +1,8 @@
+#![feature(async_await)]
+fn main() {
+    env_logger::from_env(env_logger::Env::default().default_filter_or("info")).init();
+    let mut app = tide::App::new();
+    app.middleware(tide::middleware::RequestLogger::new());
+    app.at("/").get(async move |_| "Hello, world!");
+    app.run("127.0.0.1:8000").unwrap();
+}
