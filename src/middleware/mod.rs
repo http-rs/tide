8b06916e75de05b8ba34021f07b3e897479857e3
Prasanna Loganathar <pvl@prasannavl.com>
2019-05-21 16:50:14 +0530

    isolate tide-headers, tide-slog; better; improve loggers

--- src/middleware/mod.rs
+++
@@ -1,7 +0,0 @@
-mod default_headers;
-mod logger;
-
-pub use self::{default_headers::DefaultHeaders, logger::RootLogger};
-#[cfg(feature = "cookies")]
-pub use tide_cookies::CookiesMiddleware;
-pub use tide_core::middleware::{Middleware, Next};
---
+++ tide-slog/src/lib.rs
@@ -0,0 +1,71 @@
+#![feature(async_await)]
+#![warn(
+    nonstandard_style,
+    rust_2018_idioms,
+    future_incompatible,
+    missing_debug_implementations
+)]
+
+use slog::{info, trace, o, Drain};
+use slog_async;
+use slog_term;
+
+use futures::future::BoxFuture;
+use futures::prelude::*;
+
+use tide_core::{
+    middleware::{Middleware, Next},
+    Context, Response,
+};
+
+/// RequestLogger based on slog.SimpleLogger
+#[derive(Debug)]
+pub struct RequestLogger {
+    // drain: dyn slog::Drain,
+    inner: slog::Logger,
+}
+
+impl RequestLogger {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn with_logger(logger: slog::Logger) -> Self {
+        Self { inner: logger }
+    }
+}
+
+impl Default for RequestLogger {
+    fn default() -> Self {
+        let decorator = slog_term::TermDecorator::new().build();
+        let drain = slog_term::CompactFormat::new(decorator).build().fuse();
+        let drain = slog_async::Async::new(drain).build().fuse();
+
+        let log = slog::Logger::root(drain, o!());
+        Self { inner: log }
+    }
+}
+
+/// Stores information during request phase and logs information once the response
+/// is generated.
+impl<Data: Send + Sync + 'static> Middleware<Data> for RequestLogger {
+    fn handle<'a>(&'a self, cx: Context<Data>, next: Next<'a, Data>) -> BoxFuture<'a, Response> {
+        FutureExt::boxed(async move {
+            let path = cx.uri().path().to_owned();
+            let method = cx.method().as_str().to_owned();
+            trace!(self.inner, "IN => {} {}", method, path);
+            let start = std::time::Instant::now();
+            let res = next.run(cx).await;
+            let status = res.status();
+            info!(
+                self.inner,
+                "{} {} {} {}ms",
+                method,
+                path,
+                status.as_str(),
+                start.elapsed().as_millis()
+            );
+            res
+        })
+    }
+}
