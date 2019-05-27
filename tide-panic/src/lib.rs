a081bfe3e70f9edf2091e5065f29e4f9941d741f
Wim Looman <wim@nemo157.com>
2019-05-27 19:07:28 +0200

    Add middleware for catching panics

---
+++ tide-panic/src/lib.rs
@@ -0,0 +1,18 @@
+#![feature(async_await, doc_cfg)]
+#![warn(
+    nonstandard_style,
+    rust_2018_idioms,
+    future_incompatible,
+    missing_debug_implementations,
+    missing_docs
+)]
+
+//! Advanced panic support for Tide applications.
+//!
+//! These middleware should not generally be necessary, they are provided for situations in which
+//! Tide's default panic handling is not usable by your application. Before using these you should
+//! have a good understanding of how the different components involved in [`std::panic`] works.
+
+mod catch_unwind;
+
+pub use crate::catch_unwind::CatchUnwind;
