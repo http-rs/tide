b80e2f863eeeaf3d63264d5ec7d5be35dbc471bd
Fangdun Cai <cfddream@gmail.com>
2019-10-27 10:11:41 +0800

    feat(core): If JSON Serialize failure return 500 and Error message

--- tide-core/src/response.rs
+++ tide-core/src/response.rs
@@ -7,12 +7,19 @@ pub type Response = http_service::Response;
 
 /// Serialize `t` into a JSON-encoded response.
 pub fn json<T: serde::Serialize>(t: T) -> Response {
-    // TODO: remove the `unwrap`
-    http::Response::builder()
-        .status(http::status::StatusCode::OK)
-        .header("Content-Type", "application/json")
-        .body(Body::from(serde_json::to_vec(&t).unwrap()))
-        .unwrap()
+    let mut res = http::Response::builder();
+
+    match serde_json::to_vec(&t) {
+        Ok(v) => res
+            .header("Content-Type", "application/json")
+            .body(Body::from(v)),
+        Err(e) => {
+            log::error!("{}", e);
+            res.status(http::status::StatusCode::INTERNAL_SERVER_ERROR)
+                .body(Body::empty())
+        }
+    }
+    .unwrap()
 }
 
 /// A value that is synchronously convertable into a `Response`.
@@ -121,6 +128,7 @@ impl<R: IntoResponse> IntoResponse for WithStatus<R> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use futures::executor::block_on;
 
     #[test]
     fn test_status() {
@@ -128,17 +136,49 @@ mod tests {
             .with_status(http::status::StatusCode::NOT_FOUND)
             .into_response();
         assert_eq!(resp.status(), http::status::StatusCode::NOT_FOUND);
+        assert_eq!(block_on(resp.into_body().into_vec()).unwrap(), b"foo");
     }
 
     #[test]
     fn byte_vec_content_type() {
         let resp = String::from("foo").into_bytes().into_response();
         assert_eq!(resp.headers()["Content-Type"], "application/octet-stream");
+        assert_eq!(block_on(resp.into_body().into_vec()).unwrap(), b"foo");
     }
 
     #[test]
     fn string_content_type() {
         let resp = String::from("foo").into_response();
         assert_eq!(resp.headers()["Content-Type"], "text/plain; charset=utf-8");
+        assert_eq!(block_on(resp.into_body().into_vec()).unwrap(), b"foo");
+    }
+
+    #[test]
+    fn json_content_type() {
+        use std::collections::BTreeMap;
+
+        let mut map = BTreeMap::new();
+        map.insert(Some("a"), 2);
+        map.insert(Some("b"), 4);
+        map.insert(None, 6);
+
+        let resp = json(map);
+        assert_eq!(
+            resp.status(),
+            http::status::StatusCode::INTERNAL_SERVER_ERROR
+        );
+        assert_eq!(block_on(resp.into_body().into_vec()).unwrap(), b"");
+
+        let mut map = BTreeMap::new();
+        map.insert("a", 2);
+        map.insert("b", 4);
+        map.insert("c", 6);
+
+        let resp = json(map);
+        assert_eq!(resp.status(), http::status::StatusCode::OK);
+        assert_eq!(
+            block_on(resp.into_body().into_vec()).unwrap(),
+            br##"{"a":2,"b":4,"c":6}"##
+        );
     }
 }
