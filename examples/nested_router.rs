b6de9cf87891a476796eff5a14cc9383d0932a89
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-11-03 03:28:51 +0100

    nested router example #331
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

---
+++ examples/nested_router.rs
@@ -0,0 +1,15 @@
+use tide::App;
+
+fn main() {
+    let mut app = App::new();
+    app.at("/gates").nest(|router| {
+        router
+            .at("/")
+            .get(|_| async move { "This is an area in front of the gates" });
+        router.at("/open").get(|_| async move { "Open the gates!" });
+        router
+            .at("/close")
+            .get(|_| async move { "Close the gates!" });
+    });
+    app.run("127.0.0.1:8000").unwrap();
+}
