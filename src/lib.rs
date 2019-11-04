837d5363dfc898ff730b7e0383997610fb81d8a3
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-11-04 03:11:43 +0100

    update example
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,9 @@
 //!
 //! fn main() -> Result<(), std::io::Error> {
 //!     block_on(async {
-//!         let mut app = tide::Server::new();
+//!         let mut app = tide::new();
 //!         app.at("/").get(|_| async move { "Hello, world!" });
-//!         app.listen("127.0.0.1:8000").await?;
-//!         Ok(())
+//!         app.listen("127.0.0.1:8000").await
 //!     })
 //! }
 //! ````
@@ -28,12 +27,18 @@ mod context;
 mod endpoint;
 mod route;
 mod router;
-mod server;
 
+pub mod server;
+
+#[doc(hidden)]
 pub mod cookies;
+#[doc(hidden)]
 pub mod forms;
+#[doc(hidden)]
 pub mod middleware;
+#[doc(hidden)]
 pub mod querystring;
+#[doc(hidden)]
 pub mod response;
 
 #[doc(inline)]
@@ -43,8 +48,20 @@ pub use crate::{
     error::{EndpointResult, Error},
     response::Response,
     route::Route,
-    server::{Server, Service},
+    server::Server,
 };
 
-#[doc(inline)]
 pub use http;
+
+/// Create a new Tide server.
+pub fn new() -> server::Server<()> {
+    Server::new()
+}
+
+/// Create a new Tide server with state
+pub fn with_state<State>(state: State) -> server::Server<State>
+where
+    State: Send + Sync + 'static,
+{
+    Server::with_state(state)
+}
