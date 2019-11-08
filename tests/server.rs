f0432a3d8050f3648852c902d70a08a5b0bb8df7
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-11-08 15:24:37 +0100

    responses work!
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- tests/server.rs
+++ tests/server.rs
@@ -7,15 +7,46 @@ fn hello_world() -> Result<(), surf::Exception> {
     task::block_on(async {
         let server = task::spawn(async {
             let mut app = tide::new();
-            app.at("/").get(|_| async {"hello world"});
+            app.at("/").get(|mut req: tide::Request<()>| async move {
+                assert_eq!(req.body_string().await.unwrap(), "nori".to_string());
+                tide::Response::new(tide::http::StatusCode::OK)
+                    .body_string("says hello".to_string())
+            });
             app.listen("localhost:8080").await?;
             Result::<(), surf::Exception>::Ok(())
         });
 
         let client = task::spawn(async {
             task::sleep(Duration::from_millis(100)).await;
-            let string = surf::get("localhost:8080").recv_string().await?;
-            assert_eq!(string, "hello world");
+            let string = surf::get("localhost:8080")
+                .body_string("nori".to_string())
+                .recv_string()
+                .await?;
+            assert_eq!(string, "says hello".to_string());
+            Ok(())
+        });
+
+        future::select!(server, client).await
+    })
+}
+
+#[test]
+fn echo_server() -> Result<(), surf::Exception> {
+    task::block_on(async {
+        let server = task::spawn(async {
+            let mut app = tide::new();
+            app.at("/").get(|req| async move { req });
+            app.listen("localhost:8081").await?;
+            Result::<(), surf::Exception>::Ok(())
+        });
+
+        let client = task::spawn(async {
+            task::sleep(Duration::from_millis(100)).await;
+            let string = surf::get("localhost:8081")
+                .body_string("chashu".to_string())
+                .recv_string()
+                .await?;
+            assert_eq!(string, "chashu".to_string());
             Ok(())
         });
 
