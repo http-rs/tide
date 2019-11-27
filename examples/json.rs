ed4a2f8a115d0bc0f8a0df3a4844839516427fba
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-11-27 19:02:45 +0100

    json example
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- examples/json.rs
+++ examples/json.rs
@@ -1,22 +1,26 @@
+use async_std::io;
 use async_std::task;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
 struct Cat {
-    name: String
+    name: String,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+fn main() -> io::Result<()> {
     task::block_on(async {
-        let app = tide::new();
+        let mut app = tide::new();
 
-        app.at("/submit").post(|req: tide::Request<()>| async {
-            let cat: Cat = req.body_json().await.unwrap();
-            println!("cat name: {}", cat.name);
+        app.at("/submit").post(|mut req: tide::Request<()>| {
+            async move {
+                let cat: Cat = req.body_json().await.unwrap();
+                println!("cat name: {}", cat.name);
 
-            let cat = Cat { name: "chashu".into() };
-            let res = tide::Response::new(200).body_json(&cat).unwrap();
-            res
+                let cat = Cat {
+                    name: "chashu".into(),
+                };
+                tide::Response::new(200).body_json(&cat).unwrap()
+            }
         });
 
         app.listen("127.0.0.1:8080").await?;
