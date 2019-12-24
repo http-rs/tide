99a3c40aa0a320e4f0eb731f70680b088fe345e3
Luca Palmieri <lpalmieri@truelayer.com>
2019-12-24 19:10:46 +0000

    Change the behaviour of `.query`: if the caller has not specified query parameters, try to deserialise in any case from an empty string. This allows successful deserialisation of structs which do not have mandatory fields (e.g. all fields are Option<X>).
    Return detailed error message when deserialisation fails, to help the caller debug what was wrong with their request.

--- tests/querystring.rs
+++ tests/querystring.rs
@@ -1,53 +1,87 @@
-// use futures::executor::block_on;
-// use http_service::Body;
-// use http_service_mock::make_server;
-// use tide::*;
+use async_std::io::prelude::ReadExt;
+use futures::executor::block_on;
+use http_service::Body;
+use http_service_mock::{make_server, TestBackend};
+use serde::Deserialize;
+use tide::{server::Service, IntoResponse, Request, Response, Server};
 
-// #[derive(Deserialize)]
-// struct Params {
-//     msg: String,
-// }
+#[derive(Deserialize)]
+struct Params {
+    msg: String,
+}
 
-// async fn handler(cx: crate::Request<()>) -> Result<String, Error> {
-//     let p = cx.url_query::<Params>()?;
-//     Ok(p.msg)
-// }
+#[derive(Deserialize)]
+struct OptionalParams {
+    _msg: Option<String>,
+    _time: Option<u64>,
+}
 
-// fn app() -> crate::Server<()> {
-//     let mut app = crate::Server::new();
-//     app.at("/").get(handler);
-//     app
-// }
+async fn handler(cx: Request<()>) -> Response {
+    let p = cx.query::<Params>();
+    match p {
+        Ok(params) => params.msg.into_response(),
+        Err(error) => error.into_response(),
+    }
+}
 
-// #[test]
-// fn successfully_deserialize_query() {
-//     let app = app();
-//     let mut server = make_server(app.into_http_service()).unwrap();
-//     let req = http::Request::get("/?msg=Hello")
-//         .body(Body::empty())
-//         .unwrap();
-//     let res = server.simulate(req).unwrap();
-//     assert_eq!(res.status(), 200);
-//     let body = block_on(res.into_body().into_vec()).unwrap();
-//     assert_eq!(&*body, &*b"Hello");
-// }
+async fn optional_handler(cx: Request<()>) -> Response {
+    let p = cx.query::<OptionalParams>();
+    match p {
+        Ok(_) => Response::new(200),
+        Err(error) => error.into_response(),
+    }
+}
 
-// #[test]
-// fn unsuccessfully_deserialize_query() {
-//     let app = app();
-//     let mut server = make_server(app.into_http_service()).unwrap();
-//     let req = http::Request::get("/").body(Body::empty()).unwrap();
-//     let res = server.simulate(req).unwrap();
-//     assert_eq!(res.status(), 400);
-// }
+fn get_server() -> TestBackend<Service<()>> {
+    let mut app = Server::new();
+    app.at("/").get(handler);
+    app.at("/optional").get(optional_handler);
+    make_server(app.into_http_service()).unwrap()
+}
 
-// #[test]
-// fn malformatted_query() {
-//     let app = app();
-//     let mut server = make_server(app.into_http_service()).unwrap();
-//     let req = http::Request::get("/?error=should_fail")
-//         .body(Body::empty())
-//         .unwrap();
-//     let res = server.simulate(req).unwrap();
-//     assert_eq!(res.status(), 400);
-// }
+#[test]
+fn successfully_deserialize_query() {
+    let mut server = get_server();
+    let req = http::Request::get("/?msg=Hello")
+        .body(Body::empty())
+        .unwrap();
+    let res = server.simulate(req).unwrap();
+    assert_eq!(res.status(), 200);
+    let mut body = String::new();
+    block_on(res.into_body().read_to_string(&mut body)).unwrap();
+    assert_eq!(body, "Hello");
+}
+
+#[test]
+fn unsuccessfully_deserialize_query() {
+    let mut server = get_server();
+    let req = http::Request::get("/").body(Body::empty()).unwrap();
+    let res = server.simulate(req).unwrap();
+    assert_eq!(res.status(), 400);
+
+    let mut body = String::new();
+    block_on(res.into_body().read_to_string(&mut body)).unwrap();
+    assert_eq!(body, "failed with reason: missing field `msg`");
+}
+
+#[test]
+fn malformatted_query() {
+    let mut server = get_server();
+    let req = http::Request::get("/?error=should_fail")
+        .body(Body::empty())
+        .unwrap();
+    let res = server.simulate(req).unwrap();
+    assert_eq!(res.status(), 400);
+
+    let mut body = String::new();
+    block_on(res.into_body().read_to_string(&mut body)).unwrap();
+    assert_eq!(body, "failed with reason: missing field `msg`");
+}
+
+#[test]
+fn empty_query_string_for_struct_with_no_required_fields() {
+    let mut server = get_server();
+    let req = http::Request::get("/optional").body(Body::empty()).unwrap();
+    let res = server.simulate(req).unwrap();
+    assert_eq!(res.status(), 200);
+}
