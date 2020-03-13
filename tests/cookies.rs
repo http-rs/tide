022c8ddd6d0a176711f88f8223a620e6dd24435a
Fangdun Cai <cfddream@gmail.com>
2020-03-13 16:10:23 +0800

    deps: cookie v0.13.3

--- tests/cookies.rs
+++ tests/cookies.rs
@@ -89,7 +89,7 @@ fn successfully_remove_cookie() {
     assert_eq!(cookie.name(), COOKIE_NAME);
     assert_eq!(cookie.value(), "");
     assert_eq!(cookie.http_only(), None);
-    assert_eq!(cookie.max_age().unwrap().num_nanoseconds(), Some(0));
+    assert_eq!(cookie.max_age().unwrap().whole_nanoseconds(), 0);
 }
 
 #[test]
