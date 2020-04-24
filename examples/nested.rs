ee4a707208dbcf199bbb71410ce9fc4399f3d772
Yoshua Wuyts <yoshuawuyts@gmail.com>
2020-04-24 12:35:54 +0200

    fix ci on master

--- examples/nested.rs
+++ examples/nested.rs
@@ -1,16 +1,13 @@
-use async_std::task;
-
-fn main() -> Result<(), std::io::Error> {
-    task::block_on(async {
-        let mut app = tide::new();
-        app.at("/").get(|_| async move { "Root" });
-        app.at("/api").nest({
-            let mut api = tide::new();
-            api.at("/hello").get(|_| async move { "Hello, world" });
-            api.at("/goodbye").get(|_| async move { "Goodbye, world" });
-            api
-        });
-        app.listen("127.0.0.1:8080").await?;
-        Ok(())
-    })
+#[async_std::main]
+async fn main() -> Result<(), std::io::Error> {
+    let mut app = tide::new();
+    app.at("/").get(|_| async move { Ok("Root") });
+    app.at("/api").nest({
+        let mut api = tide::new();
+        api.at("/hello").get(|_| async move { Ok("Hello, world") });
+        api.at("/goodbye").get(|_| async move { Ok("Goodbye, world") });
+        api
+    });
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
 }
