474fb6aad1e83ce3638eadd1885c70fad03ddd0b
yusuke-ota <33338085+yusuke-ota@users.noreply.github.com>
2020-05-18 21:59:04 +0900

    fix response.rs

--- tests/response.rs
+++ tests/response.rs
@@ -1,54 +1,92 @@
-// use async_std::task::block_on;
-// use tide::*;
+use async_std::io::prelude::*;
+use tide::*;
 
-// #[test]
-// fn test_status() {
-//     let resp = "foo"
-//         .with_status(http::status::StatusCode::NOT_FOUND)
-//         .into_response();
-//     assert_eq!(resp.status(), http::status::StatusCode::NOT_FOUND);
-//     assert_eq!(block_on(resp.into_body().into_vec()).unwrap(), b"foo");
-// }
+#[async_std::test]
+async fn test_status() {
+    let mut resp = Response::new(StatusCode::NotFound).body_string("foo".to_string());
+    assert_eq!(resp.status(), StatusCode::NotFound);
 
-// #[test]
-// fn byte_vec_content_type() {
-//     let resp = String::from("foo").into_bytes().into_response();
-//     assert_eq!(resp.headers()["Content-Type"], "application/octet-stream");
-//     assert_eq!(block_on(resp.into_body().into_vec()).unwrap(), b"foo");
-// }
+    let mut foo = String::new();
+    let _bytes = resp.take_body().read_to_string(&mut foo).await;
+    assert_eq!(foo.as_bytes(), b"foo");
+}
 
-// #[test]
-// fn string_content_type() {
-//     let resp = String::from("foo").into_response();
-//     assert_eq!(resp.headers()["Content-Type"], "text/plain; charset=utf-8");
-//     assert_eq!(block_on(resp.into_body().into_vec()).unwrap(), b"foo");
-// }
+#[async_std::test]
+async fn byte_vec_content_type() {
+    use async_std::io::Cursor;
+    use http_types::headers::{HeaderName, HeaderValue};
+    use std::str::FromStr;
 
-// #[test]
-// fn json_content_type() {
-//     use std::collections::BTreeMap;
+    let mut resp = Response::new(StatusCode::Ok).body(Cursor::new("foo"));
 
-//     let mut map = BTreeMap::new();
-//     map.insert(Some("a"), 2);
-//     map.insert(Some("b"), 4);
-//     map.insert(None, 6);
+    let mut header_values: Vec<HeaderValue> = Vec::new();
+    let content_type = HeaderName::from_str("Content-Type").unwrap();
+    if let Some(x) = resp.remove_header(&content_type) {
+        header_values = x;
+    }
+    assert_eq!(header_values[0], "application/octet-stream");
 
-//     let resp = json(map);
-//     assert_eq!(
-//         resp.status(),
-//         http::status::StatusCode::INTERNAL_SERVER_ERROR
-//     );
-//     assert_eq!(block_on(resp.into_body().into_vec()).unwrap(), b"");
+    let mut foo = String::new();
+    let _bytes = resp.take_body().read_to_string(&mut foo).await;
+    assert_eq!(foo.as_bytes(), b"foo");
+}
 
-//     let mut map = BTreeMap::new();
-//     map.insert("a", 2);
-//     map.insert("b", 4);
-//     map.insert("c", 6);
+#[async_std::test]
+async fn string_content_type() {
+    use http_types::headers::{HeaderName, HeaderValue};
+    use std::str::FromStr;
 
-//     let resp = json(map);
-//     assert_eq!(resp.status(), http::status::StatusCode::OK);
-//     assert_eq!(
-//         block_on(resp.into_body().into_vec()).unwrap(),
-//         br##"{"a":2,"b":4,"c":6}"##
-//     );
-// }
+    let mut resp = Response::new(StatusCode::Ok).body_string("foo".to_string());
+
+    let mut header_values: Vec<HeaderValue> = Vec::new();
+    let content_type = HeaderName::from_str("Content-Type").unwrap();
+    if let Some(x) = resp.remove_header(&content_type) {
+        header_values = x;
+    }
+    assert_eq!(header_values[0], "text/plain; charset=utf-8");
+
+    let mut foo = String::new();
+    let _bytes = resp.take_body().read_to_string(&mut foo).await;
+    assert_eq!(foo.as_bytes(), b"foo");
+}
+
+#[async_std::test]
+async fn json_content_type() {
+    use http_types::Method;
+    use std::collections::BTreeMap;
+
+    let mut app = tide::new();
+    app.at("/json_content_type").get(|_| async move {
+        let mut map = BTreeMap::new();
+        map.insert(Some("a"), 2);
+        map.insert(Some("b"), 4);
+        map.insert(None, 6);
+        println!("{:?}", serde_json::to_vec(&map));
+
+        let resp = Response::new(StatusCode::Ok).body_json(&map)?;
+        Ok(resp)
+    });
+
+    let req = http::Request::new(
+        Method::Get,
+        "http://localhost/json_content_type".parse().unwrap(),
+    );
+    let mut resp: http::Response = app.respond(req).await.unwrap();
+    assert_eq!(resp.status(), StatusCode::InternalServerError);
+
+    let mut body = String::new();
+    let _bytes = resp.take_body().read_to_string(&mut body).await;
+    assert_eq!(body.as_bytes(), b"");
+
+    let mut map = BTreeMap::new();
+    map.insert("a", 2);
+    map.insert("b", 4);
+    map.insert("c", 6);
+
+    let mut resp = Response::new(StatusCode::Ok).body_json(&map).unwrap();
+    assert_eq!(resp.status(), StatusCode::Ok);
+
+    let mut body = String::new();
+    let _bytes = resp.take_body().read_to_string(&mut body).await;
+    assert_eq!(body.as_bytes(), br##"{"a":2,"b":4,"c":6}"##);
+}
