7c02ce2892cda8aab968c45494123e963d51d41b
yusuke-ota <33338085+yusuke-ota@users.noreply.github.com>
2020-05-19 21:37:32 +0900

    Remove: println!() and some lines

--- tests/response.rs
+++ tests/response.rs
@@ -4,7 +4,6 @@ use tide::*;
 async fn test_status() {
     let mut resp = Response::new(StatusCode::NotFound).body_string("foo".to_string());
     assert_eq!(resp.status(), StatusCode::NotFound);
-
     let foo = resp.take_body().into_string().await.unwrap();
     assert_eq!(foo.as_bytes(), b"foo");
 }
@@ -16,14 +15,12 @@ async fn byte_vec_content_type() {
     use std::str::FromStr;
 
     let mut resp = Response::new(StatusCode::Ok).body(Cursor::new("foo"));
-
     let mut header_values: Vec<HeaderValue> = Vec::new();
     let content_type = HeaderName::from_str("Content-Type").unwrap();
     if let Some(x) = resp.remove_header(&content_type) {
         header_values = x;
     }
     assert_eq!(header_values[0], "application/octet-stream");
-
     let foo = resp.take_body().into_string().await.unwrap();
     assert_eq!(foo.as_bytes(), b"foo");
 }
@@ -34,14 +31,12 @@ async fn string_content_type() {
     use std::str::FromStr;
 
     let mut resp = Response::new(StatusCode::Ok).body_string("foo".to_string());
-
     let mut header_values: Vec<HeaderValue> = Vec::new();
     let content_type = HeaderName::from_str("Content-Type").unwrap();
     if let Some(x) = resp.remove_header(&content_type) {
         header_values = x;
     }
     assert_eq!(header_values[0], "text/plain; charset=utf-8");
-
     let foo = resp.take_body().into_string().await.unwrap();
     assert_eq!(foo.as_bytes(), b"foo");
 }
@@ -57,19 +52,15 @@ async fn json_content_type() {
         map.insert(Some("a"), 2);
         map.insert(Some("b"), 4);
         map.insert(None, 6);
-        println!("{:?}", serde_json::to_vec(&map));
-
         let resp = Response::new(StatusCode::Ok).body_json(&map)?;
         Ok(resp)
     });
-
     let req = http::Request::new(
         Method::Get,
         "http://localhost/json_content_type".parse().unwrap(),
     );
     let mut resp: http::Response = app.respond(req).await.unwrap();
     assert_eq!(resp.status(), StatusCode::InternalServerError);
-
     let body = resp.take_body().into_string().await.unwrap();
     assert_eq!(body.as_bytes(), b"");
 
@@ -77,10 +68,8 @@ async fn json_content_type() {
     map.insert("a", 2);
     map.insert("b", 4);
     map.insert("c", 6);
-
     let mut resp = Response::new(StatusCode::Ok).body_json(&map).unwrap();
     assert_eq!(resp.status(), StatusCode::Ok);
-
     let body = resp.take_body().into_string().await.unwrap();
     assert_eq!(body.as_bytes(), br##"{"a":2,"b":4,"c":6}"##);
 }
