af3b75f65b60a7220d0a8716ea7cc1f05cf59447
yusuke-ota <33338085+yusuke-ota@users.noreply.github.com>
2020-05-23 10:28:22 +0900

    use "map_err()" and "?" instead of "match {}"

--- tests/wildcard.rs
+++ tests/wildcard.rs
@@ -9,16 +9,8 @@ async fn add_one(cx: Request<()>) -> Result<String, tide::Error> {
 }
 
 async fn add_two(cx: Request<()>) -> Result<String, tide::Error> {
-    let one;
-    match cx.param::<i64>("one") {
-        Ok(num) => one = num,
-        Err(err) => return Err(tide::Error::new(StatusCode::BadRequest, err)),
-    };
-    let two;
-    match cx.param::<i64>("two") {
-        Ok(num) => two = num,
-        Err(err) => return Err(tide::Error::new(StatusCode::BadRequest, err)),
-    };
+    let one= cx.param::<i64>("one").map_err(|err| tide::Error::new(StatusCode::BadRequest, err))?;
+    let two= cx.param::<i64>("two").map_err(|err| tide::Error::new(StatusCode::BadRequest, err))?;
     Ok((one + two).to_string())
 }
 
