2631b9a90d8b2fb6881e865cb53e8387dccd5047
Jacob Rothstein <hi@jbr.me>
2020-06-25 13:25:54 -0700

    add an upload example

---
+++ examples/upload.rs
@@ -0,0 +1,49 @@
+use async_std::{fs::OpenOptions, io};
+use tempfile::TempDir;
+use tide::prelude::*;
+use tide::{Body, Request, Response, StatusCode};
+
+#[async_std::main]
+async fn main() -> Result<(), std::io::Error> {
+    tide::log::start();
+    let mut app = tide::with_state(tempfile::tempdir()?);
+
+    // To test this example:
+    // $ cargo run --example upload
+    // $ curl -T ./README.md locahost:8080 # this writes the file to a temp directory
+    // $ curl localhost:8080/README.md # this reads the file from the same temp directory
+
+    app.at(":file")
+        .put(|req: Request<TempDir>| async move {
+            let path: String = req.param("file")?;
+            let fs_path = req.state().path().join(path);
+
+            let file = OpenOptions::new()
+                .create(true)
+                .write(true)
+                .open(&fs_path)
+                .await?;
+
+            let bytes_written = io::copy(req, file).await?;
+
+            tide::log::info!("file written", {
+                bytes: bytes_written,
+                path: fs_path.canonicalize()?.to_str()
+            });
+
+            Ok(json!({ "bytes": bytes_written }))
+        })
+        .get(|req: Request<TempDir>| async move {
+            let path: String = req.param("file")?;
+            let fs_path = req.state().path().join(path);
+
+            if let Ok(body) = Body::from_file(fs_path).await {
+                Ok(body.into())
+            } else {
+                Ok(Response::new(StatusCode::NotFound))
+            }
+        });
+
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
+}
