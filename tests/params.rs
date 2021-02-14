4b9123d2699d93e0767851abb5964c0c6c45d9e1
Mendelt Siebenga <mendelt@mendelt.nl>
2021-02-14 21:25:27 +0100

    unit test percent decoding params

--- tests/params.rs
+++ tests/params.rs
@@ -35,5 +35,41 @@ async fn hello_world_parametrized() -> Result<()> {
     let req = http_types::Request::new(Method::Get, Url::parse("http://example.com/iron")?);
     let mut res: http_types::Response = server.respond(req).await?;
     assert_eq!(res.body_string().await?, "iron says hello");
+
+    let req = http_types::Request::new(Method::Get, Url::parse("http://example.com/nori%26iron")?);
+    let mut res: http_types::Response = server.respond(req).await?;
+    assert_eq!(res.body_string().await?, "nori&iron says hello");
+
+    Ok(())
+}
+
+#[async_std::test]
+async fn multi_segment_parameter() -> Result<()> {
+    async fn greet(req: tide::Request<()>) -> Result<impl Into<Response>> {
+        let body = req.param("param").unwrap();
+        Ok(Response::builder(200).body(body))
+    }
+
+    let mut server = tide::new();
+    server.at("/*param").get(greet);
+
+    // Should still work with one segment
+    let req = http_types::Request::new(Method::Get, Url::parse("http://example.com/one")?);
+    let mut res: http_types::Response = server.respond(req).await?;
+    assert_eq!(res.body_string().await?, "one");
+
+    // Or two
+    let req = http_types::Request::new(Method::Get, Url::parse("http://example.com/one/two")?);
+    let mut res: http_types::Response = server.respond(req).await?;
+    assert_eq!(res.body_string().await?, "one/two");
+
+    // And it should percent-decode in between the slashes
+    let req = http_types::Request::new(
+        Method::Get,
+        Url::parse("http://example.com/one/two%20three")?,
+    );
+    let mut res: http_types::Response = server.respond(req).await?;
+    assert_eq!(res.body_string().await?, "one/two three");
+
     Ok(())
 }
