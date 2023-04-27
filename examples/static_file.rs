bfee85b2532a7f4a09aabc9bcd793fb70a581907
Sijmen <me@vijf.life>
2023-04-27 13:57:39 +0200

    Fix route in static_file example

--- examples/static_file.rs
+++ examples/static_file.rs
@@ -4,7 +4,7 @@ async fn main() -> Result<(), std::io::Error> {
     let mut app = tide::new();
     app.with(tide::log::LogMiddleware::new());
     app.at("/").get(|_| async { Ok("visit /src/*") });
-    app.at("/src/*").serve_dir("src/")?;
+    app.at("/src").serve_dir("src/")?;
 
     // Make sure examples/static_file.html is available relative to the current-dir this example is run from or replace this with an absolute path.
     app.at("/example").serve_file("examples/static_file.html")?;
